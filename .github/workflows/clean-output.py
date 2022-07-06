ab4bf52d90a75e670c4eb0a0aadca26c79e1d98f
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2022-07-06 20:37:29 +0200

    build: created python script to clean the output

---
+++ .github/workflows/clean-output.py
@@ -0,0 +1,14 @@
+import re
+
+with open('output.log') as oldfile, open('error.log', 'w') as newfile:
+    for line in oldfile:
+        if not (b'\xc3\xa2\xc5\xa1\xc2\xa0' in line.encode()):
+            if ('warning' in line):
+                r = re.search('warning  (.*) remark', line)
+                s = r.group(1).rsplit('  ', 1)[0]
+                newfile.write(s)
+            if ('home/runner/work' in line):
+                s = line.replace("/home/runner/work/free-programming-books/", "", 1)
+                newfile.write(s)
+        else:
+            newfile.write("\n\n")
